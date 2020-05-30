ba24223254cfea9b6d910091011f77185cc5f1ac
sankarmuthaiah <sankar.muthaiah@gmail.com>
2020-05-30 11:21:12 +0530

    Created docker-react app

---
+++ src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import App from './App';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Welcome/i);
+  expect(linkElement).toBeInTheDocument();
+});
