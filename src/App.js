e2ea26e3bf932e00513d7c322c2039c321b9f4e6
sankarmuthaiah <sankar.muthaiah@gmail.com>
2020-05-30 13:23:48 +0530

    Changed App JS text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome !!!
+          Welcome !!! Updated in feature branch !
         </p>
       </header>
     </div>
